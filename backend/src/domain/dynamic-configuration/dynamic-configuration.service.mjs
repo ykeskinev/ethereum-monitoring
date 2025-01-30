1ec47cd3516baf74baa334dcc8950c543d827b5a
Yavor Keskinev <ykeskinev@gmail.com>
2025-01-30 22:55:19 +0200

    hardcoded create

--- backend/src/domain/dynamic-configuration/dynamic-configuration.service.mjs
+++ backend/src/domain/dynamic-configuration/dynamic-configuration.service.mjs
@@ -7,6 +7,35 @@ class DynamicConfigurationService {
 
     }
 
+    async createConfiguration() {
+        const result = await DynamicConfiguration.create(
+            {
+                name: "Detect Expensive Transactions 9",
+                rules: [
+                    {
+                        name: 'Expensive Transactions',
+                        field: 'gas',
+                        operator: '>',
+                        check_value: '10000'
+                    },
+                    {
+                        name: 'To Our address',
+                        field: 'to',
+                        operator: '=',
+                        check_value: 'dasdafafaffs'
+                    },
+                ]
+            },
+            {
+                include: [DynamicConfiguration.rules],
+            },
+        )
+
+      
+        console.log('result', result)
+        return result.dataValues
+    }
+
     async getConfigurations() {
         // const result = await dbService.query()
         return [new DynamicConfiguration()]
