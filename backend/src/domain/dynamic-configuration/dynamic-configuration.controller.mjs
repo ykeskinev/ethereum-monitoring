7cc22ded30d9da92c6ff591e298119ffb497b333
Yavor Keskinev <ykeskinev@gmail.com>
2025-01-30 22:54:29 +0200

    add endpoints for crud

--- backend/src/domain/dynamic-configuration/dynamic-configuration.controller.mjs
+++ backend/src/domain/dynamic-configuration/dynamic-configuration.controller.mjs
@@ -4,6 +4,22 @@ import { dynamicConfigurationService } from './dynamic-configuration.service.mjs
 class DynamicConfigurationController extends Controller {
 
     async init(app) {
+
+        app.post('/configuration', async (req, res) => {
+            const result = await dynamicConfigurationService.createConfiguration();
+            res.send(result)
+        })
+
+        app.put('/configuration', async (req, res) => {
+            const result = await dynamicConfigurationService.getConfigurations();
+            res.send(result)
+        })
+
+        app.get('/configuration', async (req, res) => {
+            const result = await dynamicConfigurationService.getConfigurations();
+            res.send(result)
+        })
+
         app.get('/configurations', async (req, res) => {
             const result = await dynamicConfigurationService.getConfigurations();
             res.send(result)
